f7c0ee9c9a28a3d533fe4faaed1d4c9fe4764c43
mrA121 <royshivank@gmail.com>
2020-09-24 15:33:06 +0530

    App Js Changes Created Structure for the components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Search from "./components/Search";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={SignIn} />
+          <Route path="/search" component={Search} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
